b5f6382415f499d10384c28683883b8d7031bf25
Rahul Kumar <rahul.cs.mnnit@gmail.com>
2024-10-26 15:00:02 +0530

    Updating is_tv plugin to latest flutter version

---
+++ example/android/app/src/main/kotlin/com/litedevs/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.litedevs.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/com/litedevs/is_tv_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.litedevs.is_tv_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
